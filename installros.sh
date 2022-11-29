e60144a38c023efe351a7f46527585a24211c39e
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-11-29 21:18:59 +0530

    add catkin installation function

--- installros.sh
+++ installros.sh
@@ -6,7 +6,7 @@
 #       to install/build the dependencies                                      #
 ################################################################################
 
-
+# 1. inbstalling ros noetic
 install_ros_default () {
 
     echo
@@ -44,11 +44,49 @@ install_ros_default () {
 	
 }
 
+# 2. Catkin-tools package provides command line tools for working with the catkin meta-buildsystem and catkin workspaces.
+install_catkin() {
+
+	# https://catkin-tools.readthedocs.io/en/latest/installing.html
+	# First you must have the ROS repositories which contain the .deb for catkin_tools:
+	sudo sh \
+    -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" \
+        > /etc/apt/sources.list.d/ros-latest.list'
+	
+	# Once you have added that repository, run these commands to install catkin_tools:
+	sudo apt-get update
+	sudo apt-get install python3-catkin-tools
+
+}
+
+create_ros_ws () {
+
+	# make separate dir in home dir
+	mkdir ros && cd ros
+
+	# create a workspace
+	mkdir -p new_ros_ws/src
+
+	# change the dir
+	cd new_ros_ws
+
+	# ros is already sourced from the previous installation step
+
+	# initialize catkin
+	catkin init
+}
+
 # main function 
 main () {
     
 	# installing ROS1 default building
 	install_ros_default ()
+
+	# install catkin
+	install_catkin()
+
+	# create ros workspace
+	create_ros_ws ()
     
 }
 
